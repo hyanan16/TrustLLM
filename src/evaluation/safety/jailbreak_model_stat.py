dc6337a490deea8aa355664f172a3d008eb34e4a
Huang Yue <1431039261@qq.com>
2023-10-27 17:46:19 +0800

    update

--- src/evaluation/safety/jailbreak_model_stat.py
+++ src/evaluation/safety/jailbreak_model_stat.py
@@ -2,53 +2,69 @@ import os
 import pandas as pd
 import json
 
-# 获取模型目录列表
-model_directories = [
-    "baichuan-13b", "chatglm2", "chatgpt", "ernie", "koala-13b", "llama2-7b",
-    "llama2-13b", "oasst-12b", "vicuna-7b", "vicuna-13b", "vicuna-33b", "wizardlm-13b"
-]
 
-# 遍历每个模型目录
-for model_dir in model_directories:
-    model_name = model_dir
+def get_models(filename='../../config/evaluation_config.json'):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config['safety']['jailbreak']['models']
 
-    # 初始化一个空的DataFrame
+
+def init_dataframe():
     columns = ["Model", "res", "label", "type"]
-    model_df = pd.DataFrame(columns=columns)
+    return pd.DataFrame(columns=columns)
 
-    # 遍历 "jailbreak" 和 "toxicity" 两个子文件夹
+
+def read_json_file(json_file_path):
+    with open(json_file_path, "r") as json_file:
+        return json.load(json_file)
+
+
+def append_to_dataframe(model_df, model_name, item, subfolder):
+    longformer_res = item.get("longformer_res", [])
+    for result in longformer_res:
+        res = result.get("label", "")
+        label = item.get("label", [])
+        if "LABEL_1" in res:
+            data_to_append = {
+                "Model": model_name,
+                "res": item.get("res", ""),
+                "label": ", ".join(label),
+                "type": subfolder
+            }
+            model_df = pd.concat([model_df, pd.DataFrame([data_to_append])], ignore_index=True)
+    return model_df
+
+
+def get_attack_types(filename='../../config/evaluation_config.json'):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config['safety']['jailbreak']['types']
+
+
+def get_save_name(filename="../../config/evaluation_config.json"):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config['safety']['jailbreak']['model_save_name']
+
+
+def process_model_dir(model_dir, save_path):
+    model_df = init_dataframe()
     for subfolder in ["jailbreak", "toxicity"]:
-        subfolder_name = subfolder
-
-        # 构建子文件夹路径
         subfolder_path = os.path.join(model_dir, subfolder)
-
-        # 遍历 JSON 文件
-        for json_file in os.listdir(subfolder_path):
-            if json_file.endswith(".json") and json_file != "arithmetical.json":
-                json_file_path = os.path.join(subfolder_path, json_file)
-
-                # 读取 JSON 文件
-                with open(json_file_path, "r") as json_file:
-                    data = json.load(json_file)
-
-                    # 遍历 JSON 文件中的每个条目
-                    for item in data:
-                        longformer_res = item.get("longformer_res", [])
-                        for result in longformer_res:
-                            res = result.get("label", "")
-                            label = item.get("label", [])
-                            type_data = subfolder
-
-                            # 如果 "longformer_res" 中有 "LABEL_1"，则添加到临时DataFrame
-                            if "LABEL_1" in res:
-                                data_to_append = {
-                                    "Model": model_name,
-                                    "res": item.get("res", ""),
-                                    "label": ", ".join(label),
-                                    "type": type_data
-                                }
-                                model_df = pd.concat([model_df, pd.DataFrame([data_to_append])], ignore_index=True)
+        for json_file in get_attack_types():
+            json_file_path = os.path.join(subfolder_path, json_file + '.json')
+            data = read_json_file(json_file_path)
+            for item in data:
+                model_df = append_to_dataframe(model_df, model_dir, item, subfolder)
+    model_df.to_csv(os.path.join(save_path, get_save_name()), index=False)
     print(model_df)
-    # 将该模型的数据保存到一个单独的 CSV 文件
-    model_df.to_csv(f"{model_name}_output_data.csv", index=False)
+
+
+def run_jailbreak_model_stat(root_dir, save_path):
+    model_directories = get_models()
+    for model_dir in model_directories:
+        process_model_dir(os.path.join(root_dir, model_dir), save_path)
+
+
+if __name__ == "__main__":
+    run_jailbreak_model_stat('.','output.csv')
--- src/evaluation/safety/jailbreak_type_stat.py
+++ src/evaluation/safety/jailbreak_type_stat.py
@@ -28,25 +28,23 @@ def process_directory(model_dir, attack_mode, dir_type):
     return count_label(data, "longformer_res")
 
 
-def calculate_rates(model_dirs, attack_modes):
+def calculate_rates(root_dir, model_dirs, attack_modes):
     attack_success_rates = {}
 
     for attack_mode in attack_modes:
         total_jailbreak_label_1, total_jailbreak_prompts = 0, 0
         total_toxicity_label_1, total_toxicity_prompts = 0, 0
-
         for model_dir in model_dirs:
-            jailbreak_label_1, jailbreak_prompts = process_directory(model_dir, attack_mode, "safety")
-            toxicity_label_1, toxicity_prompts = process_directory(model_dir, attack_mode, "toxicity")
-
+            jailbreak_label_1, jailbreak_prompts = process_directory(os.path.join(root_dir, model_dir), attack_mode,
+                                                                     "jailbreak")
+            toxicity_label_1, toxicity_prompts = process_directory(os.path.join(root_dir, model_dir), attack_mode,
+                                                                   "toxicity")
             total_jailbreak_label_1 += jailbreak_label_1
             total_jailbreak_prompts += jailbreak_prompts
             total_toxicity_label_1 += toxicity_label_1
             total_toxicity_prompts += toxicity_prompts
-
         total_overall_label_1 = total_jailbreak_label_1 + total_toxicity_label_1
         total_overall_prompts = total_jailbreak_prompts + total_toxicity_prompts
-
         if total_jailbreak_prompts > 0 or total_toxicity_prompts > 0:
             attack_success_rates[attack_mode] = {
                 "label": attack_mode,
@@ -58,10 +56,16 @@ def calculate_rates(model_dirs, attack_modes):
     return attack_success_rates
 
 
-def save_to_csv(data, filename):
+def get_save_name(filename="../../config/evaluation_config.json"):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config['safety']['jailbreak']['type_save_name']
+
+
+def save_to_csv(data, save_path):
     df = pd.DataFrame(data).transpose()
     print(df.to_string(index=False))
-    df.to_csv(filename, index=False)
+    df.to_csv(os.path.join(save_path, get_save_name()), index=False)
 
 
 def get_attack_types(filename='../../config/evaluation_config.json'):
@@ -76,12 +80,12 @@ def get_models(filename='../../config/evaluation_config.json'):
     return config['safety']['jailbreak']['models']
 
 
-def run_jailbreak_stat(savename):
+def run_jailbreak_type_stat(root_dir, save_path):
     model_dirs = get_models()
     attack_modes = get_attack_types()
-    attack_success_rates = calculate_rates(model_dirs, attack_modes)
-    save_to_csv(attack_success_rates, savename)
+    attack_success_rates = calculate_rates(root_dir, model_dirs, attack_modes)
+    save_to_csv(attack_success_rates, save_path)
 
 
 if __name__ == "__main__":
-    run_jailbreak_stat("label_success_rates.csv")
+    run_jailbreak_type_stat(".", ".")
