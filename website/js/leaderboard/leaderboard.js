e4d49d2c7ec4ac34ebe7460c276fe1e45aa6459d
nauyisu022 <59754221+nauyisu022@users.noreply.github.com>
2023-11-11 12:57:39 +0800

    update demo

--- website/js/leaderboard/leaderboard.js
+++ website/js/leaderboard/leaderboard.js
@@ -1,21 +1,18 @@
 let obj_to_tr = (obj) => {
     return `<tr>
         <td>${obj["#"]}</td>
-        <td style="text-align: left;"><strong>${obj.Method}</strong></td>
-        <td><a href="${obj.Source.Link}" class="ext-link" style="font-size: 16px;">${obj.Source.Source}</a></td>
-        <td>${obj.Date}</td>
-        <td>${obj["#Size"]}</td>
-        <td>${obj["#Param"]}</td>
-        <td>${obj.Avg}</td>
-        <td>${obj.NAT}</td>
-        <td>${obj.SOC}</td>
-        <td>${obj.LAN}</td>
-        <td>${obj.TXT}</td>
-        <td>${obj.IMG}</td>
-        <td>${obj.NO}</td>
-        <td>${obj["G1-6"]}</td>
-        <td>${obj["G7-12"]}</td>
-    </tr>`
+        <td style="text-align: left;"><strong>${obj.Model}</strong></td>
+        <td>${obj["Stereotype Overall"]}</td>
+        <td>${obj["Stereotype Overall_rank"]}</td>
+        <td>${obj["Overall Agreement Rate"]}</td>
+        <td>${obj["Overall Agreement Rate_rank"]}</td>
+        <td>${obj["Sex"]}</td>
+        <td>${obj["Sex_rank"]}</td>
+        <td>${obj["Race"]}</td>
+        <td>${obj["Race_rank"]}</td>
+        <td>${obj["Refuse Overall"]}</td>
+        <td>${obj["Refuse Overall_rank"]}</td>
+    </tr>`;
 }
 
 let table = document.getElementById("results");
